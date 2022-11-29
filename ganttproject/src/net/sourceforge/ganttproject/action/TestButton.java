fd6c9c37ddf2bf94baaef4546ddb3fbdec17464a
CarolinaSimonet <c.simonet@campus.fct.unl.pt>
2022-11-29 21:04:53 +0000

    button updATE

--- ganttproject/src/net/sourceforge/ganttproject/action/TestButton.java
+++ ganttproject/src/net/sourceforge/ganttproject/action/TestButton.java
@@ -10,8 +10,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 
 
-
-
 /**
  * Collection of actions from Help menu.
  *
@@ -29,46 +27,58 @@ public class TestButton extends GPAction {
         myUiFacade = uifacade;
     }
 
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            //showAboutDialog();
-            for (GPOptionGroup group : myUiFacade.getOptions()) {
-                group.lock();
-            }
-            final OkAction okAction = new OkAction() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    rollback();
-                }
-            };
+    private static void addEmptyRow(JPanel form) {
+        form.add(Box.createRigidArea(new Dimension(1, 10)));
+        form.add(Box.createRigidArea(new Dimension(1, 10)));
+    }
 
 
-            //myUiFacade.createDialog(createDialogComponent(), new Action[] { okAction }, "").show();
-
-            OptionsPageBuilder optionsBuilder = new OptionsPageBuilder();
-            optionsBuilder.setUiFacade(myUiFacade);
-            JPanel contentPanel = new JPanel(new BorderLayout());
-            //contentPanel.add(optionsBuilder.createGroupComponent(myUiFacade.getGanttChart().getBaselineColorOptions()), BorderLayout.SOUTH);
-
-            //vamos ter de fazer algo assim
-            //contentPanel.add(optionsBuilder.createGroupComponent()
-            myUiFacade.createDialog(contentPanel,new Action[] { okAction } , "dailyInformation").show();
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //showAboutDialog();
+        for (GPOptionGroup group : myUiFacade.getOptions()) {
+            group.lock();
         }
-
-
-
-
-        private void rollback() {
-            for (GPOptionGroup group : myUiFacade.getOptions()) {
-                group.rollback();
+        final OkAction okAction = new OkAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                rollback();
             }
+        };
+
+
+        //myUiFacade.createDialog(createDialogComponent(), new Action[] { okAction }, "").show();
+
+        OptionsPageBuilder optionsBuilder = new OptionsPageBuilder();
+        optionsBuilder.setUiFacade(myUiFacade);
+        JPanel contentPanel = new JPanel(new BorderLayout());
+        //contentPanel.add(optionsBuilder.createGroupComponent(myUiFacade.getGanttChart().getBaselineColorOptions()), BorderLayout.SOUTH);
+        contentPanel.add(new JLabel("Resumo das tasks"));
+        addEmptyRow(contentPanel);
+        contentPanel.add(new JLabel("Outro resumo das tasks"));
+        //vamos ter de fazer algo assim
+        //contentPanel.add(optionsBuilder.createGroupComponent()
+        myUiFacade.createDialog(contentPanel, new Action[]{okAction}, "dailyInformation").show();
+    }
+
+
+    private void rollback() {
+        for (GPOptionGroup group : myUiFacade.getOptions()) {
+            group.rollback();
         }
     }
 
 
-            //mudar para coisas da daily task
-            /* private Component createDialogComponent() {
+    public Component getComponentTest() {
+        MyTestComponent c = new MyTestComponent();
+        return c.getComponent();
+    }
+
+}
+
+
+//mudar para coisas da daily task
+      /*     private Component createDialogComponent() {
                  OptionsPageBuilder builder = new OptionsPageBuilder();
                  builder.setUiFacade(myUiFacade);
                  JComponent comp = builder.buildPage(myUiFacade.getOptions(), "dailyInformation");
